f1c4ed89feab170b8db09a0375b1dfab029988ca
Baeyung <ahmadsheikh617@gmail.com>
2022-01-12 23:43:52 +0500

    --add confirmation screens

--- 058_125_227_milestone4/main.js
+++ 058_125_227_milestone4/main.js
@@ -57,33 +57,90 @@ var foodlist = [
   },
 ];
 
-function set_item_page() {
+function add_to_cart()
+{
   var item_id = window.sessionStorage.getItem("curr_item");
-  document.getElementById("food-name").innerHTML = foodlist[parseInt(item_id)]["Name"];
-  document.getElementById("food-desc").innerHTML = foodlist[parseInt(item_id)]["Description"];
+  var quantity = document.getElementById("demoInput").value;
+  console.log(quantity);
+
+  var cart_string = window.sessionStorage.getItem("cart")
+  if (cart_string == null){
+    cart_string = "[]";
+  }
+  var prev_cart = JSON.parse(cart_string);
+  console.log(prev_cart);
+  prev_cart.push({"ID":item_id, "Quantity":quantity});
+  window.sessionStorage.setItem("cart", JSON.stringify(prev_cart));
+  window.location.href="main.html"
+}
+
+function fill_cart()
+{
+  var cart_string = window.sessionStorage.getItem("cart")
+  if (cart_string == null){
+    cart_string = "[]";
+  }
+  var prev_cart = JSON.parse(cart_string);
+
+  const fragment = document.createDocumentFragment();
+  var total_amount = 0;
+  var total_items = 0;
+  for (var i in prev_cart) {
+    const li = document.createElement("li");
+    li.innerHTML = '<div class="product"> <img src=' + foodlist[parseInt(prev_cart[i]["ID"])]["Pic"] + ' width="80", height="80">'+
+    '<p id="item-desc">'+ foodlist[parseInt(prev_cart[i]["ID"])]["Name"]+' , half</p> <p id="single-price"> Price = '+ foodlist[parseInt(prev_cart[i]["ID"])]["Price"] + '</p>\
+    <button id="minus" onclick="decrement()">-</button>\
+    <input id="demoInput" type="number" min="1" max="100", value='+prev_cart[i]["Quantity"]+'>\
+    <button id="plus"onclick="increment()">+</button>\
+    <p id="all-price">Total =' + (parseInt(foodlist[parseInt(prev_cart[i]["ID"])]["Price"]) * parseInt(prev_cart[i]["Quantity"])).toString() +'</p></div>'
+    fragment.appendChild(li);
+
+    total_amount += (parseInt(foodlist[parseInt(prev_cart[i]["ID"])]["Price"]) * parseInt(prev_cart[i]["Quantity"]));
+    total_items+=1;
+
+  }
+  const container = document.getElementById("overview-list-item");
+  document.getElementById("total-item-cost").innerHTML=total_amount.toString();
+  document.getElementById("total-item-grand-cost").innerHTML=total_amount.toString();
+  document.getElementById("no-of-products").innerHTML= total_items.toString() + ' items in cart'; 
+  container.innerHTML = "";
+  container.appendChild(fragment);
+}
+
+function make_order()
+{
+  window.sessionStorage.setItem("cart","[]");
+  window.location.href="main.html"
+}
+
+function set_item_page()
+{
+  var item_id=window.sessionStorage.getItem("curr_item");
+  document.getElementById("food-name").innerHTML=foodlist[parseInt(item_id)]["Name"];
+  document.getElementById("food-desc").innerHTML=foodlist[parseInt(item_id)]["Description"];
 
 }
 
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
-
+  
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
 
 function increment() {
-  document.getElementById('demoInput').stepUp();
+    document.getElementById('demoInput').stepUp();
 }
-
+  
 function decrement() {
-  document.getElementById('demoInput').stepDown();
+    document.getElementById('demoInput').stepDown();
 }
 
-function gotoitem(img_id) {
+function gotoitem(img_id){
   var x = img_id.id;
-  window.sessionStorage.setItem("curr_item", x);
+  window.sessionStorage.setItem("curr_item",x);
   window.location.href = "item.html"
 }
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ The first milestone was to do user analysis, task analysis, and domain analysis
 The second milestone was about creating the persona's, making wireframes in different manner and then analyzing and creating story boarding for the wire frames
 # Milestone 3
 In third Milestone we created high fidelity prototype that is Interactive and user can perform actions. It is not the exact replica of the final product but somewhat similar.
+# Milestone 4
+In this milestone we have implemented a task using the HTML, CSS, JavaScript. The task implemented was for user in which he can order the food via browsing in the static menu. 
+After that a detailed discription of the item is shown in which he can either add special instructions and add to cart.
+Further he can also view the cart containing the items he added to the cart, and submit the order.
